9a4dfc4acb5134a85c6b17a62539141b40c5502b
Johannes Brodwall <johannes@brodwall.com>
2022-09-05 12:42:02 +0200

    Score pair (#3)

--- src/test/java/no/kristiania/poker/PokerHandsTest.java
+++ src/test/java/no/kristiania/poker/PokerHandsTest.java
@@ -28,4 +28,28 @@ class PokerHandsTest {
         });
         assertEquals("high card 10", hand.getScore());
     }
+
+    @Test
+    void shouldIdentifyPairOfTwo() {
+        var hand = new PokerHand(new Card[] {
+                new Card(Suit.HEART, Rank.NUMBER_2),
+                new Card(Suit.HEART, Rank.NUMBER_3),
+                new Card(Suit.SPADES, Rank.NUMBER_2),
+                new Card(Suit.CLUBS, Rank.NUMBER_8),
+                new Card(Suit.DIAMONDS, Rank.NUMBER_10),
+        });
+        assertEquals("pair of 2", hand.getScore());
+    }
+
+    @Test
+    void shouldIdentifyPairOfAce() {
+        var hand = new PokerHand(new Card[] {
+                new Card(Suit.HEART, Rank.ACE),
+                new Card(Suit.HEART, Rank.NUMBER_3),
+                new Card(Suit.SPADES, Rank.NUMBER_2),
+                new Card(Suit.CLUBS, Rank.ACE),
+                new Card(Suit.DIAMONDS, Rank.NUMBER_10),
+        });
+        assertEquals("pair of Ace", hand.getScore());
+    }
 }
